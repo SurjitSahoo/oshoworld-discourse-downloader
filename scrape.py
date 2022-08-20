d56c2ee6ccc076ea023c81dce9b638212cc04593
Surjit Kumar Sahoo <surjitsahoo0@gmail.com>
2022-08-20 17:41:31 +0530

    added on-disk network caching

--- scrape.py
+++ scrape.py
@@ -1,12 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import os.path
+import json
 from pySmartDL import SmartDL
 
-base_dir = 'downloads'
+download_dir = 'downloads'
+cache_dir = 'cache'
 
-if not os.path.exists(base_dir):
-	os.makedirs(base_dir)
+if not os.path.exists(download_dir):
+	os.makedirs(download_dir)
+
+if not os.path.exists(cache_dir):
+	os.makedirs(cache_dir)
 
 headers = {
 	 "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
@@ -16,9 +21,31 @@ baseURL = 'https://oshoworld.com/audio-discourse-hindi-'
 
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def cached_get(url: str, is_json=False):
+	fileName = url.split('/')[-2] if url.endswith('/') else url.split('/')[-1]
+	if fileName.startswith('?'):
+		fileName = fileName[1:]
+	filePath = os.path.join(os.getcwd(), cache_dir, fileName + '.json' if is_json else fileName + '.html')
+
+	if os.path.isfile(path=filePath):
+		if is_json:
+			with open(filePath, 'r') as f:
+				return json.load(f)
+		else:
+			return open(filePath, mode='rt', encoding='utf-8').read()
+	else:
+		req = requests.get(url, headers=headers)
+		content = req.json() if json else requests.get(url, headers=headers).text
+		with open(filePath, mode='wt', encoding='utf-8') as f:
+			if is_json:
+				f.write(json.dumps(content))
+			else:
+				f.write(content)
+		return content
+
 def download_discourse(url: str, base_dir: str):
 	print('Finding tracks for Discourse ' + url + '...')
-	soup = BeautifulSoup((requests.get(url, headers=headers).content), 'html.parser')
+	soup = BeautifulSoup(cached_get(url), 'html.parser')
 	discourse = soup.find('meta', attrs={'property': 'og:title'})['content'].split(' # ')[0]
 	discourse_dir = base_dir + '/' + discourse
 	
@@ -26,7 +53,7 @@ def download_discourse(url: str, base_dir: str):
 		os.makedirs(discourse_dir)
 
 	audio_igniter = soup.find('div', attrs={'class': 'audioigniter-root'})['data-tracks-url']
-	tracks = requests.get(audio_igniter, headers=headers).json()
+	tracks = cached_get(url=audio_igniter, is_json=True)
 	for track in tracks:
 		fileName = track['downloadFilename'] or track['title'] + '.mp3'
 		downloadURL = track['downloadUrl'] or track['audio']
@@ -44,29 +71,18 @@ def download_discourse(url: str, base_dir: str):
 	
 
 for alphabet in alphabets:
-	dir_alpha = base_dir + '/' + alphabet
+	dir_alpha = download_dir + '/' + alphabet
+
 	if not os.path.exists(dir_alpha):
 		os.makedirs(dir_alpha)
 
-	file_path = os.path.join(os.getcwd(), base_dir, alphabet + '.txt')
-	
-	if os.path.isfile(file_path):
-		with open(file_path, mode='r', encoding='utf-8') as file:
-			print('Found discourses with ' + alphabet.capitalize() + ' in: ' + alphabet + '.txt \n')
-			for line in file:
-				download_discourse(line, dir_alpha)
+	print('Finding discourses with ' + alphabet.capitalize() + '...')
+	url = baseURL + alphabet + '/'
+	soup = BeautifulSoup(cached_get(url), 'html.parser')
+	discourses = soup.find_all('a', string="Play & Download", href=True)
+	discourse_links = [discourse['href'] for discourse in discourses]
 
-	else:
-		print('Finding discourses with ' + alphabet.capitalize() + '...')
-		url = baseURL + alphabet + '/'
-		soup = BeautifulSoup((requests.get(url, headers=headers).content), 'html.parser')
-		discourses = soup.find_all('a', string="Play & Download", href=True)
-		discourse_links = [discourse['href'] for discourse in discourses]
-
-		with open(file_path, mode='wt', encoding='utf-8') as file:
-			file.write('\n'.join(discourse_links))
-
-		for discourse in discourse_links:
-			download_discourse(discourse, dir_alpha)
+	for discourse in discourse_links:
+		download_discourse(discourse, dir_alpha)
 
 print('>>>> Completed all downloads! <<<<')
